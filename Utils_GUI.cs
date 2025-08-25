24aad2eed89cc1107bccc84810e7ffcaac6bbfc1
mitasamodel <mitasamodel@gmail.com>
2025-08-25 15:26:46 +0200

    init

---
+++ Utils_GUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RW_Utils
+{
+	public static class Utils_GUI
+	{
+		public const float buttonHeigt = 30f;
+		public const float gapHeight = 12f;
+		public const float resetButtonAreaHeight = buttonHeigt + gapHeight;
+		public const float rowHeight = 22f;
+
+		public static void DrawBox(Rect rect, Color color, int thickness = 1)
+		{
+			GUI.color = color;
+			Widgets.DrawBox(rect, thickness);
+			GUI.color = Color.white;
+		}
+		public static void GapLine(float x, float y, float width)
+		{
+			GUI.color = Color.gray;
+			Widgets.DrawLineHorizontal(x, y, width);
+			GUI.color = Color.white;
+		}
+		public static bool ResetButton(Rect inRect, string label)
+		{
+			Utils_GUI.GapLine(inRect.x, inRect.yMax - buttonHeigt - gapHeight / 2f, inRect.width);
+			return Widgets.ButtonText(new Rect(inRect.x, inRect.yMax - buttonHeigt, inRect.width, buttonHeigt), label);
+		}
+
+		public static string LabeledTextField(Listing_Standard listing, string label, string value, float labelWidth = 120f, float gap = 6f)
+		{
+			Rect row = listing.GetRect(22f);
+
+			Rect labelRect = new Rect(row.x, row.y, labelWidth, row.height);
+			Rect fieldRect = new Rect(row.x + labelWidth + gap, row.y, row.width - labelWidth - gap, row.height);
+
+			Widgets.Label(labelRect, label);
+			return Widgets.TextField(fieldRect, value ?? "");
+		}
+
+		public static bool SetWrap(bool set)
+		{
+			var wrap = Text.WordWrap;
+			Text.WordWrap = set;
+			return wrap;
+		}
+
+		public static void RestoreWrap(bool wrap)
+		{
+			Text.WordWrap = wrap;
+		}
+
+		public static void LabelCentered(Rect inRect, string label)
+		{
+			var oldAnchor = Text.Anchor;
+			Text.Anchor = TextAnchor.MiddleCenter;
+			Widgets.Label(inRect, label);
+			Text.Anchor = oldAnchor;
+		}
+	}
+}
